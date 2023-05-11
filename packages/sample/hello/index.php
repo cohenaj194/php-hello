290c8527ebb812fe38a84186affa97153812b1c5
cohenaj194 <cohenaj194@gmail.com>
2023-05-10 23:09:01 -0400

    try if statement

--- packages/sample/hello/index.php
+++ packages/sample/hello/index.php
@@ -2,12 +2,27 @@
  
 function main(array $args) : array
 {
-    $name = $args["name"] ?? "class 630";
+    // $name = $args["name"] ?? "class 630";
     
-    $greeting = "Hello {$name}!";
-    echo $greeting;
+    // $greeting = "Hello {$name}!";
+    // echo $greeting;
+
+    // if statements
+    $num_oranges = 26;
+    $num_bananas = 31;
+    $greeting = "";
+    if(($num_oranges > 25) && ($num_bananas > 30)){
+      $greeting = "25% Discount<br>";
+    } elseif(($num_oranges > 30) || ($num_bananas > 35)){
+      $greeting = "15% Discount<br>";
+    } elseif(!(($num_oranges < 5)) || (!($num_bananas < 5))){
+      $greeting = "5% Discount<br>";
+    } else {
+      $greeting = "No Discount<br>";
+    }
  
     return [
         'body' => $greeting,
     ];
+
 }
